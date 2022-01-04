11ba0bad05a2d98cedde4e5b5030f8aaa9f688eb
Cole Frank <cole.v.frank@gmail.com>
2022-01-04 11:01:06 -0600

    updated photo-query

--- photo-query.sh
+++ photo-query.sh
@@ -11,3 +11,6 @@ sxphotos export ~/Desktop/TESTING123 --db ~/Pictures/Photos\ Library.photoslibra
 osxphotos export ~/Desktop/TESTING123 --db ~/Pictures/Photos\ Library.photoslibrary --from-date "2021-01-01" --to-date "2022-01-03" --convert-to-jpeg --jpeg-quality 0.5 --selfie --only-photos
 
 osxphotos export ~/Desktop/TESTING123 --db ~/Pictures/Photos\ Library.photoslibrary --from-date "2022-01-01" --to-date "2022-01-03" --convert-to-jpeg --jpeg-quality 0.5 --query-eval photo.width==3024 --query-eval photo.height==4032
+
+osxphotos export ~/Documents/Projects/averaging_photos/avg-photos/images --db ~/Pictures/Photos\ Library.photoslibrary --from-date "2021-01-01" --to-date "2022-01-01" --convert-to-jpeg --jpeg-quality 0.5 --query-eval photo.width==3024 --query-eval photo.height==4032
+
